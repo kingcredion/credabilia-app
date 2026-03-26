010512cd7e7b1c99354f73c485e2b098232f342f
David Ortiz <KingCredion@credabilia.com>
2026-03-26 01:12:50 -0700

    fix routing

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.credabilia.app',
+  appName: 'Credabilia',
+  webDir: 'dist'
+};
+
+export default config;
